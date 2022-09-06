3b09dc452807494b8a5ac7847f462337676117e0
jiwoo <usekang24@gmail.com>
2022-09-06 11:42:27 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.scss';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './routes/Home';
+import About from './routes/About';
+import Navigation from './components/Nevigation';
 
 
-class App extends React.Component {
-  state = {
-    isLoading:  true,
-    movies: [],
-  } //루트(root)개념
-  getMovies = async () => {
-    const {
-      data:{
-        data:{movies},
-      },
-    }=await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating'); //==console.log(movies.data.data.movies)
-    // console.log(movies)
-    this.setState({movies, isLoading: false})
-  }
-  componentDidMount(){
-    this.getMovies();
-  }
-  render() {
-    const {isLoading, movies} = this.state;
-    return (
-      
-      <section className='container'>
-        {isLoading ? (<div className='loader'>
-            <span className='loader_text'>Loding...</span>
-          </div>) : (
-          <div className='movies'>
-            {movies.map((movie) => (
-              // console.log(movie)
-              <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
-            ))}
-          </div>)
-        }
-      </section>
-    );
-  }
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />} ></Route>
+          <Route path='/about/' element={<About />} ></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App;
